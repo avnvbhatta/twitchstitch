183d971e2683ae4aa0a404aa393929b7b1d69cc5
abhinav <avnvbhatta@gmail.com>
2020-06-06 16:10:34 -0400

    Updates

---
+++ precache-manifest.f10ac289b639f005dd9bdf3284b73b1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b283ac0741b24c0d5aa84cb92fb0f1",
+    "url": "/twitchstitchtest/index.html"
+  },
+  {
+    "revision": "c1146507af3c16198ff9",
+    "url": "/twitchstitchtest/static/css/main.eca754b8.chunk.css"
+  },
+  {
+    "revision": "e4fc26ea2509ef646f29",
+    "url": "/twitchstitchtest/static/js/2.1ff964f0.chunk.js"
+  },
+  {
+    "revision": "4f1074383207016e7b8212bac46ab5ee",
+    "url": "/twitchstitchtest/static/js/2.1ff964f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1146507af3c16198ff9",
+    "url": "/twitchstitchtest/static/js/main.2e29286f.chunk.js"
+  },
+  {
+    "revision": "f86e80dd938eccc4a50e",
+    "url": "/twitchstitchtest/static/js/runtime-main.ebd4d7d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/twitchstitch/precache-manifest.49b743c3599ba8062aa8ca8b1163797d.js"
+  "/twitchstitchtest/precache-manifest.f10ac289b639f005dd9bdf3284b73b1f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/twitchstitch/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/twitchstitchtest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
